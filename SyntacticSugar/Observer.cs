1a763a1dd0f9add0656a2200fd7d19b34e2da025
lxljh398 <513248126@qq.com>
2019-02-15 08:40:34 +0800

    Lx (#7)
    * Test
    
    * test
    
    * 0
    
    * push
    
    * Swagger与Exceptionless
    
    * api
    
    * test
    
    * test
    
    * 比较方法
    
    * demo
    
    * Extend

--- SyntacticSugar/Observer.cs
+++ SyntacticSugar/Observer.cs
@@ -16,22 +16,7 @@ namespace SyntacticSugar
         }
     }
 
-    /// <summary>
-    /// List扩展方法
-    /// </summary>
-    public static class ListExtend
-    {
-        //声明自定义泛型委托
-        public delegate void PrintT<T>(T t);
-
-        public static void TEach<T>(this List<T> list, PrintT<T> pt)
-        {
-            foreach (T t in list)
-            {
-                pt(t);
-            }
-        }
-    }
+    
 
     class Heater
     {
