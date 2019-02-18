3d500676443500a082cb33d6c53ea1649849b58a
lixin <513248126@qq.com>
2019-02-18 09:57:13 +0800

    demo

---
+++ PollyTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Polly;
+using Polly.Timeout;
+using System;
+using System.Threading;
+
+namespace PollyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Policy policytimeout = Policy.Timeout(3, TimeoutStrategy.Pessimistic);
+            Policy policyFallBack = Policy.Handle<TimeoutRejectedException>()
+                .Fallback(() =>
+                {
+                    Console.WriteLine("熔断降级");
+                });
+            Policy policy = policyFallBack.Wrap(policytimeout);
+            policy.Execute(() =>
+            {
+                Console.WriteLine("完成任务");
+                Thread.Sleep(5000);
+                Console.WriteLine("完成任务");
+            });
+            Console.ReadKey();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
