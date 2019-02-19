b3f1dba6d7cb49060402c85fc28f1b5f32ba7045
lxljh398 <513248126@qq.com>
2019-02-19 09:10:57 +0800

    Lixin (#9)
    * demo
    
    * demo

---
+++ AOP/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order() { Id = 1, Name = "lee", Count = 10, Price = 100.00, Desc = "订单测试" };
+            IOrderProcessor orderprocessor = new OrderProcessorDecorator(new OrderProcessor());
+            orderprocessor.Submit(order);
+            Console.ReadLine();
+        }
+
+        public class User
+        {
+            public string Name { set; get; }
+            public string PassWord { set; get; }
+        }
+
+
+
+        #region 订单
+        public class Order
+        {
+            public int Id { set; get; }
+            public string Name { set; get; }
+            public int Count { set; get; }
+            public double Price { set; get; }
+            public string Desc { set; get; }
+        }
+
+        public interface IOrderProcessor
+        {
+            void Submit(Order order);
+        }
+
+        public class OrderProcessor : IOrderProcessor
+        {
+            public void Submit(Order order)
+            {
+                Console.WriteLine("提交订单");
+            }
+        }
+
+
+        public class OrderProcessorDecorator : IOrderProcessor
+        {
+            public IOrderProcessor OrderProcessor { get; set; }
+            public OrderProcessorDecorator(IOrderProcessor orderprocessor)
+            {
+                OrderProcessor = orderprocessor;
+            }
+            public void Submit(Order order)
+            {
+                PreProceed(order);
+                OrderProcessor.Submit(order);
+                PostProceed(order);
+            }
+            public void PreProceed(Order order)
+            {
+                Console.WriteLine("提交订单前，进行订单数据校验....");
+                if (order.Price < 0)
+                {
+                    Console.WriteLine("订单总价有误，请重新核对订单。");
+                }
+            }
+
+            public void PostProceed(Order order)
+            {
+                Console.WriteLine("提交带单后，进行订单日志记录......");
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "提交订单，订单名称：" + order.Name + "，订单价格：" + order.Price);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Domain/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        //定义应用程序域
+        public AppDomain Domain;
+        public int ThreadId;
+        //设置值
+        public void SetDomainData(string vName, string vValue)
+        {
+            Domain.SetData(vName, (object)vValue);
+            ThreadId = AppDomain.GetCurrentThreadId();
+        }
+        //获取值
+        public string GetDomainData(string name)
+        {
+            return (string)Domain.GetData(name);
+        }
+        public static void DomainMainGo()
+        {
+            string DataName = "MyData";
+            string DataValue = "Some Data to be stored";
+
+            Console.WriteLine("Retrieving current domain");
+            MyAppDomain Obj = new MyAppDomain();
+            Obj.Domain = AppDomain.CurrentDomain;
+
+            Console.WriteLine("Setting domain data");
+            Obj.SetDomainData(DataName, DataValue);
+
+            Console.WriteLine("Getting domain data");
+            Console.WriteLine($"The Data found for key{DataName},is{Obj.GetDomainData(DataName)},running on thread id: {Obj.ThreadId}");
+        }
+    }
+}
